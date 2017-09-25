791bccd98a7fb33fab704a66563d9990f59fb4ca
Liam Cripwell <ljcripwell@gmail.com>
2017-09-25 15:21:35 +1000

    refactoring

--- filetag.go
+++ filetag.go
@@ -18,12 +18,14 @@ func main() {
 	records := getRecordsFromFile("data.json")
 	existing := -1
 
+	// check whether a record alreadu exists for file
 	for i, record := range records {
 		if record.FileName == os.Args[1] {
 			existing = i
 		}
 	}
 
+	// update records with new content
 	if existing < 0 {
 		newFile := Record{os.Args[1], os.Args[2:]}
 		records = append(records, newFile)
@@ -32,16 +34,14 @@ func main() {
 			append(records[existing].Tags, os.Args[2:]...)
 	}
 
+	// write updated data to file
 	writeRecordsToFile(records, "data.json")
 	fmt.Println("Done...")
 }
 
 func getRecordsFromFile(file string) []Record {
 	// read in file
-	b, err := ioutil.ReadFile(file)
-	if err != nil {
-		fmt.Print(err)
-	}
+	b, _ := ioutil.ReadFile(file)
 	jsonContent := string(b)
 
 	// parse the JSON
@@ -55,7 +55,7 @@ func writeRecordsToFile(records []Record, file string) {
 	// open file to write data to
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println("Can't data open file...")
+		fmt.Println("Can't open data file...")
 		return
 	}
 	defer f.Close()
